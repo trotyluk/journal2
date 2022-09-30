1f4bb6b227f1e3771e651be9278bc2567e51d9f9
trotyluk <trotylpl@gmail.com>
2022-09-30 20:58:05 +0100

    nowy oczatek

--- journal/views.py
+++ journal/views.py
@@ -1,8 +1,33 @@
 from django.http import HttpResponse
 
 from django.shortcuts import render
+
+data = {
+    'journals': [
+        {
+            'id': 5,
+            'fname': 'Dominik',
+            'sname': 'Trochonowicz',
+            'dob': '29-11-2022'
+        },
+        {
+            'id': 6,
+            'fname': 'Jan',
+            'sname': 'Kochanowski',
+            'dob': '15-02-1976'
+        },
+        {
+            'id': 7,
+            'fname': 'Grzegorz',
+            'sname': 'Trochonowicz',
+            'dob': '15-01-1971'
+        }
+        
+        ]   
+}
+
 def journal(request):
-    return render(request, 'journal/journal.html')
+    return render(request, 'journal/journal.html', data)
 
 def home(request):
     return HttpResponse('Home page')
